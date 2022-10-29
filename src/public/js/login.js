bccb4af665439f9a853dbc266b9112defbb29e7b
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2022-10-29 14:28:44 -0300

    Login responsive

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -1,11 +1,20 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-
+const signUpButtonResponsive = document.getElementById('signUpResponsive');
+const signInButtonResponsive = document.getElementById('signInResponsive')
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
 
+signUpButtonResponsive.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
 signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+signInButtonResponsive.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
